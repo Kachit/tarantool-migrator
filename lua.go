16c85d121bdd8e0d13af7db487c00913f0000e3b
Kachit <antoxachaos@gmail.com>
2024-10-07 18:24:45 +0300

    initial

--- lua.go
+++ lua.go
@@ -14,14 +14,14 @@ func NewLuaMigration(id string, upRequest string, downRequest string) *Migration
 	return &Migration{
 		ID: id,
 		Migrate: func(tt pool.Pooler, ctx context.Context, opts *Options) error {
-			_, err := tt.Do(tarantool.NewEvalRequest(upRequest), opts.WriteMode).Get()
+			_, err := tt.Do(tarantool.NewEvalRequest(upRequest).Context(ctx), opts.WriteMode).Get()
 			if err != nil {
 				return err
 			}
 			return nil
 		},
 		Rollback: func(tt pool.Pooler, ctx context.Context, opts *Options) error {
-			_, err := tt.Do(tarantool.NewEvalRequest(downRequest), opts.WriteMode).Get()
+			_, err := tt.Do(tarantool.NewEvalRequest(downRequest).Context(ctx), opts.WriteMode).Get()
 			if err != nil {
 				return err
 			}
